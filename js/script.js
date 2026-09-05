79a13e50eea831ff9b6565d7349c82b67be7e32c
faisalrizkigunawan-droid <faisal.rizkigunawan@gmail.com>
2026-09-05 11:11:44 +0700

    menambahkan search form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-// toggle class active
+// toggle class active untuk hamburger menu
 const navbarnav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
-// klik diluar sidebar untuk menghilangkan nav
+// toggle class active untuk search form
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchform.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+// klik diluar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector("#search-button");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
+  }
+  if (!searchbutton.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
   }
 });
